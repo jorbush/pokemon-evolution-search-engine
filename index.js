0439e19d867feed8c5e0e4050663a5d71daa4b1f
Jordi <55429631+jorbush@users.noreply.github.com>
2023-07-19 20:59:29 +0200

    Desist dockerfile

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import weaviate from 'weaviate-ts-client';
 
 const client = weaviate.client({
-    scheme: 'https',
-    host: 'image-search-engine.onrender.com/',
+    scheme: 'http',
+    host: 'localhost:8080',
 });
 
 const schemaRes = await client.schema.getter().do();
